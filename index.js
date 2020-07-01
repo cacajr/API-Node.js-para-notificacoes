ebfcb674fa9dbae4768980a8dfea46d8e2b0d9c3
CacaJr <espinhado010203@gmail.com>
2020-07-01 01:31:19 -0300

    creating the SMS service

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+require('dotenv/config');
+
 const express = require('express');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER;
+const client = require('twilio')(accountSid, authToken);
 
 const app = express();
 
 app.use(express.json());
+
+// Serviço de envio de SMS via Twilio
+app.get('/sms', (req, res) => {
+    const number = req.body.number
+    const message = req.body.message
+
+    client.messages
+    .create({
+        body: message,
+        from: twilioNumber,
+        to: number
+    })
+    .then(message => console.log(message.sid));
+
+    res.json({ status: 'Your message was sent!' });
+})
 
 app.listen(3333);
